0b4735f9fccff451983aeea517641b50a2a92cb4
mustafagokkurt <mustafa.gokkurt1071@gmail.com>
2018-01-08 22:28:36 +0200

    gfdjkdf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { TestLibraryModule } from 'ngx-cli-library';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TestLibraryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
